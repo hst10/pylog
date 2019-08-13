670b933044f7f2795b0cc6e0dfee6fd501a2acf6
Sitao Huang <shuang91@illinois.edu>
2019-08-13 00:42:15 -0500

    Pass config to visit methods

--- codegen_test.py
+++ codegen_test.py
@@ -38,7 +38,35 @@ def inst_lambda(lambda_node, param2arg, id):
 
 #             return 
 #         elif 
-#         return 
+#         return
+
+
+
+class Variable:
+    def __init__(self, name=None, offset=None, index=None):
+        self.name = name
+        self.offset = offset
+        self.index = index
+    def set_name(self, name):
+        self.name = name
+    def set_offset(self, offset):
+        self.offset = offset
+    def set_index(self, index):
+        self.index = index
+
+class ASTEditor(ast.NodeTransformer):
+    def visit_Subscript(self, subscript_nd):
+        binary_op = ast.BinOp(
+            left = ast.copy_location(ast.Name(id='i', ctx=subscript_nd.ctx), subscript_nd),
+            op = ast.Add(), 
+            right = subscript_nd.value
+            )
+        return ast.copy_location(ast.Subscript(
+            value=binary_op,
+            slice=subscript_nd.slice,
+            ctx=subscript_nd.ctx
+        ), subscript_nd)
+
 
 class LogicPy(ast.NodeVisitor):
     def __init__(self):
@@ -56,17 +84,22 @@ class LogicPy(ast.NodeVisitor):
         print("arg_ids = ", arg_ids)
         map_size = self.array_sizes[arg_ids[0]]
         param_ids = []
-        map_op = call_nd.args[0]
+        hmap_op = call_nd.args[0]
         loop_idx = 'i'
 
-        if isinstance(map_op, ast.Lambda):
-            param_ids = [ elem.arg for elem in map_op.args.args]
-            print(param_ids)
+        if isinstance(hmap_op, ast.Lambda):
+            param_ids = [ elem.arg for elem in hmap_op.args.args]
             param2arg = dict(zip(param_ids, arg_ids))
-            print(param2arg)
-            loop_body = target_id + "[%s] = " % loop_idx + inst_lambda(map_op, param2arg, loop_idx)
+            loop_body = target_id + "[%s] = " % loop_idx + inst_lambda(hmap_op, param2arg, loop_idx)
 
             print("loop_body = ", loop_body)
+
+            hmap_op = ASTEditor().visit(hmap_op)
+
+            new_loop_body = target_id + "[%s] = " % loop_idx + inst_lambda(hmap_op, param2arg, loop_idx)
+
+            print("new_loop_body = ", new_loop_body)
+
             
         #     loop = gen_for_loop(map_size, loop_idx, loop_body)
         #     self.logic_ast.append(loop)
@@ -106,6 +139,8 @@ class LogicPy(ast.NodeVisitor):
     def visit_Assign(self, node):
         target_id = node.targets[0].id
 
+        print("type of assign node", type(node))
+
         if isinstance(node.value, ast.List):
             self.array_sizes[target_id] = len(node.value.elts)
             print('Found list: %s, %d' % (target_id, self.array_sizes[target_id]))
@@ -121,11 +156,39 @@ class LogicPy(ast.NodeVisitor):
     def codegen(self):
         print(self.logic_ast)
 
+
+class ast_visitor_test(ast.NodeVisitor):
+    def visit(self, node, config=None):
+        """Visit a node."""
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node, config)
+
+    def generic_visit(self, node, config=None):
+        """Called if no explicit visitor function exists for a node."""
+        for field, value in ast.iter_fields(node):
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, ast.AST):
+                        self.visit(item, config)
+            elif isinstance(value, ast.AST):
+                self.visit(value, config)
+
+    def visit_Name(self, node, config=None):
+        print("found Name node: ", node.id)
+        print("Type: ", type(node))
+        if config != None:
+            print("MESSAGE = ", config)
+        return node.id
+    def visit_BinOp(self, node, config=None):
+        print("BinOp, type: ", type(node))
+        ret = self.visit(node.left, "Hello!!! ")
+
 if __name__ == "__main__":
     if len(sys.argv) < 2: 
         print("Usage: %s test.py" % __file__ )
         # src_file = open("./tests/test2.py")
-        src_file = open("./tests/test_conv.py")
+        src_file = open("./tests/test_conv_2d.py")
     else:
         src_file = open(sys.argv[1])
     src = src_file.read()
@@ -134,10 +197,17 @@ if __name__ == "__main__":
     ast_py = ast.parse(src)
     astpretty.pprint(ast_py)
 
-    gen_inst = LogicPy()
-    gen_inst.visit(ast_py)
+    print(type(ast_py))
+
+    # gen_inst = LogicPy()
+    # gen_inst.visit(ast_py)
 
     print("Input code: ")
     print(src)
     print("Output code: ")
-    gen_inst.codegen()
+    # gen_inst.codegen()
+
+    print("Testing ast visitor: ")
+
+    ast_visitor_test_inst = ast_visitor_test()
+    ast_visitor_test_inst.visit(ast_py)
