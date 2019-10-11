bd449d00772eff9b53db428dee87e8049dc03252
Sitao Huang <shuang91@illinois.edu>
2019-10-11 02:26:01 -0500

    Add new test cases

---
+++ tests/test_matmul.py
@@ -0,0 +1,18 @@
+from numpy import dot
+from numpy import matmul
+import numpy as np
+
+a = [[1, 3, 5], [1, 3, 5], [1, 3, 5]]
+b = [[2, 4, 6], [2, 4, 6], [2, 4, 6]]
+c = dot(a, b)
+print(c)
+
+d = matmul(a, b)
+print(d)
+
+d = matmul(a, np.matrix.transpose(np.asarray(b)))
+print(d)
+
+e = [ [ dot(a_row, b_row) for b_row in b ] for a_row in a ]
+print(e)
+
